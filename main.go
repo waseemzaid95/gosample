d16695cb04fef710b471c30f9e349d355774a562
waseemzaid95 <111122654+waseemzaid95@users.noreply.github.com>
2022-11-12 15:39:36 +0500

    initialization

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Hello world, the web server
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	nameHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Zaid was here\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/name", nameHandler)
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
